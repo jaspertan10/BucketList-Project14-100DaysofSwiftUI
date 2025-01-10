540c83d529f615370162bf8bd29b62fb978f699f
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2025-01-10 12:08:04 -0800

    Add biometric sandbox testing in BiometricAuthenticationView struct

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -18,19 +18,51 @@ struct ContentView: View {
 
 struct BiometricAuthenticationView: View {
     
+    @State private var isUnlocked = false
     
     var body: some View {
         
-        
-        Text("Hello World")
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
     }
     
     func authenticate() {
         /*  1. Create instance of LAContext, allows us to query biometric status and perform authentication check
             2. Ask that context if it's capable of performing biometric authentication - Important as iPodTouch users have neither Touch ID nor face ID
             3. If biometrics are possible, kick off the actual request for authentication, passing in a closure to run when authentication completes
-         
+            4. When user has been authenticated or not, completion closure is called and will tell us whether it worked or not, and if not what the error was
          */
+        
+        
+        let context = LAContext() // 1. Create LAContext
+        var error: NSError?
+        
+        // 2. check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) { //key note, you need to pass an &. $ is for in-out. & is for reference
+            
+            let reason = "We need to unlock your data"
+            
+            // 3. kick off actual request for authentication
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                
+                // authentication has now completed
+                if success {
+                    //Authenticated successfully
+                    isUnlocked = true
+                } else {
+                    
+                }
+            }
+        } else {
+            // no biometrics available - need another method to authenticate user then. Perhaps prompting passcode?
+        }
+        
     }
 }
 
